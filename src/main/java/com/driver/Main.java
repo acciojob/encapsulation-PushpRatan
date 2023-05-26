bd6d9cc65c34b2df5a924f740e59fe992138ef59
PushpRatan <pushpratan1@gmail.com>
2023-05-26 21:22:01 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly name = new RWOnly();
+        name.setName("Pushp Ratan");
+        System.out.println(name.getName());
+    }
 }
